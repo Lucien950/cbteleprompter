450190d238189f0b175795f599c76b697744afcb
Edwin <jessicawu414@gmail.com>
2022-04-02 13:17:08 -0400

    Refactor + More Speeds

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,51 @@
 import type { NextPage } from 'next'
 import Head from "next/head"
 import { FormEvent, useEffect, useState, MouseEvent} from 'react'
-import speed from "../util/speed"
+import speed, { minSpeed, maxSpeed } from "../util/speed"
 import speedType from "../types/speed"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Home: NextPage = () => {
+	// ONMOUNT
+	useEffect(() => {
+		document.addEventListener("keypress", keyListener, false)
+		document.addEventListener("paste", () => resizeElement(), false)
+
+		// localstorage mounting
+		if (localStorage.getItem("speed")) changeSpeed(parseInt(localStorage.getItem("speed")!))
+		else localStorage.setItem("speed", speedVal.toString())
+		if (localStorage.getItem("fontSize")) changeFontSize(parseInt(localStorage.getItem("fontSize")!))
+		else localStorage.setItem("fontSize", fontSize.toString())
+	}, [])
+
 	let [isScrolling, setIsScrolling] = useState<boolean>()
 	let [scrollTimeout, setScrollTimeout] = useState <ReturnType<typeof setTimeout>|undefined> ()
 	let [speedVal, setSpeedVal] = useState<number>(3)
 	let [speedObj, setSpeedObj] = useState<speedType>(() => speed(speedVal))
 	let [fontSize, setFontSize] = useState<number>(48)
 	const [hack, setHack] = useState<boolean>(false)
-
 	const [minFontSize, maxFontSize] = [0, 100]
-	const [minSpeed, maxSpeed] = [0, 16]
+	const endNotif = () => { toast.info('End reached', { position: "top-right", autoClose: 3000, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, }) }
+	// everytime update speed, stop and go
+	useEffect(() => {if(isScrolling){stop();forward();}}, [speedObj])
 
-
-	// Textbox resizing
-	// when font size is changing
-	const fontSizeInput = (e: MouseEvent, up: boolean)=>{
-		e.preventDefault()
-		const newFontSize = up ? fontSize + 4 : fontSize - 4
-		changeFontSize(newFontSize)
+	const resizeElement = (textArea?: HTMLInputElement) => {
+		if (!textArea) {
+			textArea = document.getElementById("textArea") as HTMLInputElement
+		}
+		textArea.style.height = "0px"
+		textArea.style.height = (textArea.scrollHeight + 1).toString() + "px"
 	}
+	
+	// Util changespeed
+	const changeSpeed = (newSpeed: number) => {
+		if (newSpeed < minSpeed || newSpeed > maxSpeed) return
+		localStorage.setItem("speed", newSpeed.toString())
+		setSpeedVal(newSpeed)
+		setSpeedObj(speed(newSpeed))
+	}
+	// change font size util function
 	const changeFontSize = (newFontSize: number) => {
 		if(newFontSize < minFontSize || newFontSize > maxFontSize) return
 		const textarea = document.getElementById('textArea') as HTMLInputElement
@@ -35,65 +56,37 @@ const Home: NextPage = () => {
 		// Hack to fix textarea resizing
 		resizeElement(textarea)
 	}
-
-	// everytime the form is changed
-	const resizeElementInput = (e: FormEvent) => {
-		resizeElement(e.target as HTMLInputElement)
-	}
-	// backend resize function
-	const resizeElement = (textArea?: HTMLInputElement) => {
-		if (!textArea){
-			textArea = document.getElementById("textArea") as HTMLInputElement
-		}
-		textArea.style.height = "0px"
-		textArea.style.height = (textArea.scrollHeight + 1).toString() + "px"
-	}
-
-	// Scrolling
-	const pause = () => {
-		// don't touch isScrolling if it has not been set yet
-		// prevents stop() from being called on first render
-		if(typeof isScrolling === 'undefined') return
-		setIsScrolling(false)
-	}
-	const play = () => { setIsScrolling(true) }
-
-	const scrollDown = (offset: number) => {
-		const scrollContainer = document.getElementById("scrollContainer")!
-
-		//don't scroll to far
-		if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
-			// TODO end reached
-			toast.info('End reached', {
-				position: "top-right",
-				autoClose: 3000,
-				hideProgressBar: true,
-				closeOnClick: true,
-				pauseOnHover: true,
-				draggable: true,
-				progress: undefined,
-			});
-			setIsScrolling(false)
-			return
-		}
-
-		// scroll now
-		scrollContainer.scrollTop += offset
-
-		// if stopped, don't scroll anymore
-		if (!isScrolling) return
-
-		// recursion
-		setHack(oldHack => !oldHack)
-		setScrollTimeout(setTimeout(() => { scrollDown(speedObj.step) }, speedObj.scspeed))
-	}
+	// util forward 
 	const forward = ()=>{
+		const scrollDown = (offset: number) => {
+			const scrollContainer = document.getElementById("scrollContainer")!
+
+			//don't scroll to far
+			if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
+				endNotif()
+				setIsScrolling(false)
+				return
+			}
+
+			// scroll now
+			scrollContainer.scrollTop += offset
+
+			// if stopped, don't scroll anymore
+			console.log(isScrolling)
+			if (!isScrolling) return
+
+			// recursion
+			setHack(oldHack => !oldHack)
+			setScrollTimeout(setTimeout(() => { scrollDown(speedObj.step) }, speedObj.scspeed))
+		}
+
+		console.log("forward")
 		setScrollTimeout(setTimeout(() => { scrollDown(speedObj.step) }, speedObj.scspeed))
 	}
-
+	// util stop button
 	const stop = ()=>{
 		if(scrollTimeout){
-			clearTimeout(scrollTimeout!)
+			clearTimeout(scrollTimeout)
 			setScrollTimeout(undefined)
 		}
 		else{
@@ -101,26 +94,6 @@ const Home: NextPage = () => {
 		}
 	}
 
-	// runs when key is pressed
-	const keyListener = (e: KeyboardEvent) => {
-		if (e.code === "Space") {
-			if (document.activeElement?.tagName !== "BODY") return
-			e.preventDefault()
-			setIsScrolling(oldVal => !oldVal)
-		}
-	}
-
-	useEffect(() => {
-		document.addEventListener("keypress", keyListener, false)
-		document.addEventListener("paste", ()=>resizeElement(), false)
-
-		if (localStorage.getItem("speed")) changeSpeed(parseInt(localStorage.getItem("speed")!))
-		else localStorage.setItem("speed", speedVal.toString())
-		if (localStorage.getItem("fontSize")) changeFontSize(parseInt(localStorage.getItem("fontSize")!))
-		else localStorage.setItem("fontSize", fontSize.toString())
-	}, [])
-
-	// add listener at mount
 	useEffect(()=>{
 		if(typeof isScrolling === "undefined") return
 		if (isScrolling){
@@ -129,15 +102,7 @@ const Home: NextPage = () => {
 			if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
 				// TODO end reached
 				console.log("end reached")
-				toast.info('End reached', {
-					position: "top-right",
-					autoClose: 3000,
-					hideProgressBar: true,
-					closeOnClick: true,
-					pauseOnHover: true,
-					draggable: true,
-					progress: undefined,
-				});
+				endNotif()
 				setIsScrolling(false)
 				return
 			}
@@ -149,28 +114,43 @@ const Home: NextPage = () => {
 			stop()
 		}
 	}, [isScrolling])
+	
 
 
-	const changeSpeedInput = (e: MouseEvent, up: boolean)=>{
+	// INPUT EVENTS
+	// resize element from typing
+	const resizeElementInput = (e: FormEvent) => {
+		resizeElement(e.target as HTMLInputElement)
+	}
+	// frontend speed input
+	const changeSpeedInput = (e: MouseEvent, up: boolean) => {
 		e.preventDefault()
 		const newSpeed = speedVal + (up ? 1 : -1)
 		changeSpeed(newSpeed)
 	}
-
-	const changeSpeed = (newSpeed: number)=>{
-		if(newSpeed < minSpeed || newSpeed > maxSpeed) return
-		localStorage.setItem("speed", newSpeed.toString())
-		setSpeedVal(newSpeed)
-		setSpeedObj(speed(newSpeed))
+	// Textbox resizing, when font size is changing
+	const fontSizeInput = (e: MouseEvent, up: boolean) => {
+		e.preventDefault()
+		const newFontSize = up ? fontSize + 4 : fontSize - 4
+		changeFontSize(newFontSize)
 	}
-
-	useEffect(()=>{
-		if(isScrolling){
-			stop()
-			forward()
+	// runs when key is pressed
+	const keyListener = (e: KeyboardEvent) => {
+		if (e.code === "Space") {
+			if (document.activeElement?.tagName !== "BODY") return
+			e.preventDefault()
+			setIsScrolling(oldVal => !oldVal)
 		}
-	}, [speedObj])
-	
+	}
+	// Scrolling
+	const pause = () => {
+		// don't touch isScrolling if it has not been set yet
+		// prevents stop() from being called on first render
+		if (typeof isScrolling === 'undefined') return
+		setIsScrolling(false)
+	}
+	const play = () => { setIsScrolling(true) }
+
 	return (
 		<>
 			<Head>
@@ -183,12 +163,16 @@ const Home: NextPage = () => {
 				<div className="absolute top-2 w-[93%] z-10 center-x bg-white p-2 rounded-md text-lg flex flex-row items-center gap-x-4">
 					{/* play and pause */}
 					<div className="flex flex-row">
-						<svg onClick={play} className={"w-10 h-10 transition-[fill] duration-150 cursor-pointer " + (isScrolling ? "fill-gray-200" : "fill-gray-700")} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
-						</svg>
-						<svg onClick={pause} className={"w-10 h-10 transition-[fill] duration-150 cursor-pointer " + (isScrolling ? "fill-gray-700" : "fill-gray-200")} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-						</svg>
+						<button onClick={play} disabled={isScrolling}>
+							<svg className={"w-10 h-10 transition-[fill] duration-150 " + (isScrolling ? "fill-gray-200" : "fill-gray-700")} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+								<path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
+							</svg>
+						</button>
+						<button onClick={pause} disabled={!isScrolling}>
+							<svg className={"w-10 h-10 transition-[fill] duration-150 " + (isScrolling ? "fill-gray-700" : "fill-gray-200")} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+								<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+							</svg>
+						</button>
 					</div>
 
 					{/* Scroll Speed */}
@@ -211,7 +195,7 @@ const Home: NextPage = () => {
 				</div>
 
 				{/* Main Text Area */}
-				<div className={"h-screen flex justify-center pt-20 overflow-y-scroll" + (isScrolling ? " overflow-y-hidden":"")} id="scrollContainer">
+				<div className="h-screen flex justify-center pt-20 overflow-y-scroll" id="scrollContainer">
 					<textarea
 						id="textArea"
 						className="focus:outline-none focus:ring-2 resize-none
--- util/speed.ts
+++ util/speed.ts
@@ -1,79 +1,34 @@
 import speedType from "../types/speed"
 
+const minSpeed = 1
+const maxSpeed = 19
 const setspeed = (spind: number): speedType => {
     let step, scspeed;
-
-    if (spind < 1) spind = 1;
-    if (spind > 16) spind = 16;
-
+    // clamp
+    spind = Math.min(Math.max(spind, minSpeed), maxSpeed)
     switch (spind) {
-        case 1:
-            step = 1;
-            scspeed = 50;
-            break;
-        case 2:
-            step = 2;
-            scspeed = 40;
-            break;
-        case 3:
-            step = 2;
-            scspeed = 30;
-            break;
-        case 4:
-            step = 2;
-            scspeed = 25;
-            break;
-        case 5:
-            step = 2;
-            scspeed = 20;
-            break;
-        case 6:
-            step = 3;
-            scspeed = 40;
-            break;
-        case 7:
-            step = 3;
-            scspeed = 30;
-            break;
-        case 8:
-            step = 3;
-            scspeed = 22;
-            break;
-        case 9:
-            step = 3;
-            scspeed = 15;
-            break;
-        case 10:
-            step = 3;
-            scspeed = 10;
-            break;
-        case 11:
-            step = 4;
-            scspeed = 15;
-            break;
-        case 12:
-            step = 5;
-            scspeed = 10;
-            break;
-        case 13:
-            step = 5;
-            scspeed = 5;
-            break;
-        case 14:
-            step = 7;
-            scspeed = 5;
-            break;
-        case 15:
-            step = 10;
-            scspeed = 5;
-            break;
-        case 16:
-            step = 10;
-            scspeed = 2;
-            break;
+        case 1: step = 1; scspeed = 50; break; // 20 px/sec
+        case 2: step = 1; scspeed = 40; break; // 25 px/sec
+        case 3: step = 1; scspeed = 33; break; // 33 px/sec
+        case 4: step = 1; scspeed = 25; break; // 40 px/sec 
+        case 5: step = 2; scspeed = 40; break; // 50 px/sec
+        case 6: step = 2; scspeed = 30; break; // 66.6px/sec
+        case 7: step = 3; scspeed = 40; break; // 75 px/sec
+        case 8: step = 2; scspeed = 25; break; // 80 px/sec
+        case 9: step = 2; scspeed = 20; break; // 100 px/sec
+        case 10: step = 3; scspeed = 30; break; // 100 px/sec
+        case 11: step = 3; scspeed = 22; break; // 136.36 px/sec
+        case 12: step = 3; scspeed = 15; break; // 200 px/sec
+        case 13: step = 3; scspeed = 10; break;// 300px/sec
+        case 14: step = 4; scspeed = 15; break;// 266.66px/sec
+        case 15: step = 5; scspeed = 10; break;// 500px/sec
+        case 16: step = 5; scspeed = 5; break; // 1000px/sec
+        case 17: step = 7; scspeed = 5; break; // 1400px/sec
+        case 18: step = 10; scspeed = 5; break;// 2000px/sec
+        case 19: step = 10; scspeed = 2; break;// 5000px/sec
     }
-
     return { step,scspeed } as speedType;
 }
 
 export default setspeed;
+export { minSpeed, maxSpeed };
